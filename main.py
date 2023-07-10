ccae99213c306da4f91378a8f03f019e67427981
Himanshu Sekhar <hsmaharaj1@gmail.com>
2023-07-11 00:42:51 +0530

    Added some functionalies to the Buttons

--- main.py
+++ main.py
@@ -5,13 +5,32 @@ import socketserver
 import subprocess
 import re
 import http.server
+import shutil
 
-from PyQt5.QtWidgets import QWidget
+# These two classes are for Multi-Threading Purposes
+class WorkerSignals(QtCore.QObject):
+    finished = QtCore.pyqtSignal()
+
+class Worker(QtCore.QRunnable):
+    def __init__(self, fun, *args, **kwargs):
+        super(Worker, self).__init__()
+        self.fun = fun
+        self.args = args
+        self.kwargs = kwargs
+        self.signals = WorkerSignals()
+    
+    @QtCore.pyqtSlot()
+    def run(self):
+        self.fun(*self.args, **self.kwargs)
+        self.signals.finished.emit()
+
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         uic.loadUi('./res/template.ui', self)
+
+        self.pool = QtCore.QThreadPool.globalInstance() # For Multithreading Purpose
 
         self.sendbtn.installEventFilter(self)
         self.selectbtn.installEventFilter(self)
@@ -19,8 +38,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.sendbtn.clicked.connect(self.send_func)
         self.selectbtn.clicked.connect(self.select_func)
 
-    def sever_start(self):
-        directory = "./"
+    def server_start(self):
+        directory = "./temp_dir"
         server_address = ('', 8000)
         os.chdir(directory)
 
@@ -30,8 +49,12 @@ class MainWindow(QtWidgets.QMainWindow):
         with socketserver.TCPServer(server_address, handler) as httpd:
             print(f"Serving HTTP on {server_address[0]}:{server_address[1]} from '{directory}'...")
             httpd.serve_forever()
+            return
 
     def send_func(self):
+        # self.server_start()
+        worker_server = Worker(self.server_start)
+        self.pool.start(worker_server)
         self.ip_add.clear()
         result = subprocess.run(["netsh", "interface", "ip", "show", "addresses"], capture_output=True, text=True)
 
@@ -40,13 +63,28 @@ class MainWindow(QtWidgets.QMainWindow):
 
         if ipv4_address:
             print("IPv4 Address:", ipv4_address.group(1))
-            self.ip_add.setText(ipv4_address.group(1))
+            self.address = ipv4_address.group(1) + ':8000' 
+            self.ip_add.setText(self.address)
         else:
             print("IPv4 Address not found.")
 
     
     def select_func(self):
-        return
+        file_url, _ = QtWidgets.QFileDialog.getOpenFileUrl(self, "Select Files")
+        file_path = file_url.toLocalFile()
+        print(file_path)
+
+        #Copying to a new Directory
+        temp_dir = os.path.join(os.getcwd(), "temp_dir")
+        if os.path.exists(temp_dir):
+            shutil.rmtree(temp_dir)
+
+        os.makedirs(temp_dir, exist_ok=True)
+
+        new_file_path = os.path.join(temp_dir, os.path.basename(file_path))
+        shutil.copy2(file_path, new_file_path)
+
+        print("File Copied to: ", new_file_path)
 
 App = QtWidgets.QApplication(sys.argv)
 window = MainWindow()
